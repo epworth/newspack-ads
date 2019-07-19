e10414b1b7ac767b4ae01d9235709b4f4d3c381e
Philip John <phil@philipjohn.me.uk>
2019-07-19 21:09:21 +0100

    Very very very rough PoC for constructing GAM HEAD code from ad unit code.

--- src/blocks/ad-unit/view.php
+++ src/blocks/ad-unit/view.php
@@ -27,6 +27,12 @@ function newspack_ads_render_block_ad_unit( $attributes ) {
 		$ad_unit['code'] /* TODO: escape with wp_kses() */
 	);
 
+	// Detect a GAM ad and add to header code.
+	$header_code = newspack_generate_gam_header_code_for_ad_unit( $ad_unit['code'] );
+	if ( $header_code ) {
+		$content .= '<!-- ' . $header_code . ' -->';
+	}
+
 	Newspack_Ads_Blocks::enqueue_view_assets( 'ad-unit' );
 
 	return $content;
@@ -49,3 +55,57 @@ function newspack_ads_register_ad_unit() {
 	);
 }
 add_action( 'init', 'newspack_ads_register_ad_unit' );
+
+/**
+ * Generates the ad-specific head code.
+ *
+ * @param  string $ad_code The specific ad unit code.
+ * @return string          The HEAD code.
+ */
+function newspack_generate_gam_header_code_for_ad_unit( $ad_code ) {
+	$ad_unit_name       = '';
+	$ad_unit_dimensions = '';
+	$ad_unit_gpt_id     = '';
+
+	// Find the ad unit name.
+	preg_match(
+		'/--\s(\/[0-9]+\/[a-zA-Z_-]+)\s--/',
+		$ad_code,
+		$ad_unit_name
+	);
+	if ( ! isset( $ad_unit_name[1] ) ) {
+		return false;
+	}
+
+	// Find the ad unit dimensions.
+	preg_match(
+		'/width: ([0-9]+)px; height: ([0-9]+)px/',
+		$ad_code,
+		$ad_unit_dimensions
+	);
+	if ( ! isset( $ad_unit_dimensions[1] ) ) {
+		return false;
+	}
+
+	// Find the ad unit GPT thingy.
+	preg_match(
+		'/div-gpt-ad-([0-9]+-[0-9]+)/',
+		$ad_code,
+		$ad_unit_gpt_id
+	);
+	if ( ! isset( $ad_unit_gpt_id[1] ) ) {
+		return false;
+	}
+
+	// Construct the ad-specific header code.
+	$header_code = sprintf(
+		"googletag.defineSlot('%s', [%d, %d], 'div-gpt-ad-%s').addService(googletag.pubads());",
+		$ad_unit_name[1], // Ad unit code, as defined in GAM.
+		$ad_unit_dimensions[1], // Width.
+		$ad_unit_dimensions[2], // Height.
+		$ad_unit_gpt_id[1] // The GPT ID.
+	);
+
+	return $header_code;
+
+}
